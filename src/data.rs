7256c7964bd1ed2ba7c4a4f2ef01cebc4fe1e41b
Ponas <mykolas.peteraitis@gmail.com>
2020-10-26 18:58:42 +0200

    initial commit

---
+++ src/data.rs
@@ -0,0 +1,63 @@
+// The structure of data saved in between app launches
+
+use serde::{Deserialize, Serialize};
+use std::default::Default;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Data {
+    pub discord_token: Option<String>,
+}
+
+impl Default for Data {
+    fn default() -> Self {
+        Self {
+            discord_token: None,
+        }
+    }
+}
+
+impl Data {
+    pub fn load() -> Result<Self, String> {
+        // check if file exists
+        let filepath = get_path().join("data");
+        let file = if filepath.exists() {
+            // Loads the data from the system
+            match File::open(filepath) {
+                Ok(f) => f,
+                Err(e) => return Err(format!("{}", e)),
+            }
+        } else {
+            // Just return the default data
+            return Ok(Self::default());
+        };
+
+        match bincode::deserialize_from(file) {
+            Ok(d) => Ok(d),
+            Err(e) => Err(format!("{}", e)),
+        }
+    }
+    pub fn save(&self) -> Result<(), String> {
+        // make sure the directory exists
+        std::fs::create_dir_all(get_path()).unwrap();
+
+        // Saves the data to the system
+        let file = match File::create(get_path().join("data")) {
+            Ok(f) => f,
+            Err(e) => return Err(format!("{}", e)),
+        };
+
+        match bincode::serialize_into(file, self) {
+            Ok(()) => Ok(()),
+            Err(e) => Err(format!("{}", e)),
+        }
+    }
+}
+
+fn get_path() -> PathBuf {
+    directories_next::ProjectDirs::from("", "", "Oxycord")
+        .unwrap()
+        .data_dir()
+        .to_path_buf()
+}
---
+++ src/main.rs
@@ -0,0 +1,169 @@
+#![allow(unused_imports)]
+
+use gio::prelude::*;
+use gtk::prelude::*;
+use std::cell::RefCell;
+use std::env::args;
+use std::rc::Rc;
+use std::sync::Arc;
+use tokio::prelude::*;
+use tokio::runtime::Runtime;
+
+mod data;
+
+fn build_login_ui(window: &gtk::ApplicationWindow, runtime: Arc<Runtime>) {
+    let login_vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+
+    let logo = gtk::Image::from_file("icon.svg");
+    logo.set_halign(gtk::Align::Center);
+    logo.set_margin_bottom(50);
+    logo.set_margin_top(50);
+    login_vbox.add(&logo);
+
+    let email_ex = gtk::Label::new(Some("Email"));
+    email_ex.set_halign(gtk::Align::Start);
+    login_vbox.add(&email_ex);
+
+    let email = gtk::Entry::new();
+    email.set_placeholder_text(Some("john.smith@mail.com"));
+    email.set_margin_bottom(25);
+    login_vbox.add(&email);
+
+    let pass_ex = gtk::Label::new(Some("Password"));
+    pass_ex.set_halign(gtk::Align::Start);
+    login_vbox.add(&pass_ex);
+
+    let password = gtk::Entry::new();
+    let invis_char = match password.get_invisible_char() {
+        Some(c) => c,
+        None => '*',
+    };
+    password.set_placeholder_text(Some(&invis_char.encode_utf8(&mut [0u8; 4]).repeat(16)));
+    password.set_margin_bottom(25);
+    password.set_visibility(false); // hide characters, because its a password
+    login_vbox.add(&password);
+
+    let button = gtk::Button::with_label("Login");
+    let window_clone = window.clone();
+    let login_vbox_clone = login_vbox.clone();
+    let email_clone = email.clone();
+    let password_clone = password.clone();
+    button.connect_clicked(move |_s| {
+        // check if fields have been filled
+        if email_clone.get_text_length() == 0 {
+            email_clone.set_placeholder_text(Some("Please insert email!"));
+            return;
+        }
+        if password_clone.get_text_length() == 0 {
+            password_clone.set_placeholder_text(Some("Please insert password!"));
+            return;
+        }
+        // change to spinning animation
+        window_clone.remove(&login_vbox_clone);
+        build_waiting_ui(&window_clone, runtime.clone());
+
+        // try retrieving the token
+
+        #[derive(serde::Serialize)]
+        struct LoginData {
+            email: String,
+            password: String,
+            undelete: bool,
+            captcha_key: Option<()>,
+            login_source: Option<()>,
+            gift_code_sku_id: Option<()>,
+        }
+
+        let client = reqwest::Client::new();
+        let res = futures::executor::block_on(
+            runtime.spawn(
+                client
+                    .get("https://google.com/")
+                    // .post("https://discord.com/api/v8/auth/login")
+                    // .json(&LoginData {
+                    //     email: email_clone.get_text().as_str().to_string(),
+                    //     password: password_clone.get_text().as_str().to_string(),
+                    //     undelete: false,
+                    //     captcha_key: None,
+                    //     login_source: None,
+                    //     gift_code_sku_id: None,
+                    // })
+                    .send(),
+            ),
+        )
+        .unwrap();
+        println!("{:?}", res);
+    });
+    login_vbox.add(&button);
+
+    window.add(&login_vbox);
+    window.show_all();
+}
+
+fn build_waiting_ui(window: &gtk::ApplicationWindow, runtime: Arc<Runtime>) {
+    // The spinning icon page
+
+    let waiting_vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    waiting_vbox.set_valign(gtk::Align::Center);
+
+    let waiting_label = gtk::Label::new(Some("Logging in..."));
+    waiting_label.set_halign(gtk::Align::Center);
+    waiting_label.set_margin_top(25);
+    waiting_label.set_margin_bottom(25);
+    waiting_vbox.add(&waiting_label);
+
+    let spinner = gtk::Spinner::new();
+    spinner.set_halign(gtk::Align::Center);
+    spinner.set_margin_bottom(25);
+    spinner.start();
+    waiting_vbox.add(&spinner);
+
+    window.add(&waiting_vbox);
+    window.show_all();
+}
+
+fn main() {
+    let data = Rc::new(RefCell::new(data::Data::load().unwrap()));
+    println!("{:?}", data);
+
+    let runtime = {
+        let (sender, receiver) = std::sync::mpsc::sync_channel(0);
+        std::thread::spawn(move || {
+            let runtime = Arc::new(
+                tokio::runtime::Builder::new_current_thread()
+                    .enable_all()
+                    .build()
+                    .unwrap(),
+            );
+
+            sender.send(runtime.clone()).unwrap();
+        });
+
+        receiver.recv().unwrap()
+    };
+
+    let application = gtk::Application::new(Some("oxycord.oxycord"), Default::default())
+        .expect("GTK application initialization failed.");
+
+    let data_clone = data.clone();
+    application.connect_activate(move |app| {
+        let window = gtk::ApplicationWindow::new(app);
+        window.set_title("Oxycord Login");
+        window
+            .set_icon_from_file("icon.svg")
+            .expect("failed to load icon");
+        window.set_border_width(10);
+        window.set_position(gtk::WindowPosition::Center);
+        window.set_default_size(350, 0);
+
+        match &data_clone.borrow().discord_token {
+            Some(token) => {
+                build_waiting_ui(&window, runtime.clone());
+                // try connecting
+            }
+            None => build_login_ui(&window, runtime.clone()),
+        }
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+}
